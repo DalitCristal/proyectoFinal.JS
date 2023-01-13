1f148665f422165689ced151a5f7b19a69e1348c
proyecto_final <dalacristal@gmail.com>
2023-01-13 15:48:17 -0300

    Corrigiendo un bucle

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,4 +1,40 @@
 
+// The function validacion() le pasa como parametro un string validado a la function inicio()
+function validacion(string) {
+    if (!string) {
+        console.log(`"${acumularInfo}" no es un carácter valido`);
+    } else if (string === 'redes' || string === 'redes sociales' || string === 'recomendacion') {
+        console.log(`Gracias! saber que usted se conecto con nosotros a través de "${acumularInfo}", nos permite seguir brindando un mejor servicio`); 
+        nosotros()
+    } else if (string === 'otro'){
+        let otroLlegada = prompt(`Por favor, nos especificaria por donde conecto con nosotros`)
+        console.log(`Gracias! saber que usted se conecto con nosotros a través de "${otroLlegada}", nos permite seguir brindando un mejor servicio`); 
+    }
+    return string
+}
+
+// Functon inicio() se encarga de guiar al cliente hacia su compra deseada en caso de que si conozca plantisfulsoul y en caso de que no, guarda informacion para saber como llegan los clientes al vivero y luego los invita a la "seccion nosotros" para que nos conozcan
+let acumularInfo = ''
+const inicio = () => {
+    let ask = prompt(`¿Ya nos conocias?
+    Responde por si o no`).toLowerCase()
+    if (ask === 'si') {
+        console.log(`Muy felices de que vuelvas! 
+        ahora..`);
+        return seccionTienda(numTienda())
+    } else if (ask === 'no') {
+        acumularInfo = prompt(`¿Cómo nos conoció? redes sociales, recomendacion u otro?`).toLowerCase()
+        validacion(acumularInfo)
+    } else {
+        console.log(`El caracter ingresado: "${ask}" no es valido`);
+    }
+}
+
+// function nosotros() dirige al cliente hacia la seccion "Nosotros"
+function nosotros() {
+    console.log(`Bienvenido a la seccion donde te contaremos sobre Nosotros!`);
+}
+
 // The function numTienda() le pasa el parametro a function seccionTienda()
 let numTienda = () => {
     let a = Number(prompt(`¿QUÉ AREA DESEA INSPECCIONAR?
@@ -62,6 +98,12 @@ const compra = (planta) => {
         return `No ha ingresado caracteres correctos`
     } else if (planta === 'lechuga' || planta === 'tomate' || planta === 'alcaucil' || planta === 'berenjena' || planta === 'pimiento' || planta === 'zanahoria' || planta === 'romero' || planta === 'albahaca' || planta === 'lavanda' || planta === 'salvia' || planta === 'tomillo' || planta === 'incienso' || planta === 'menta' || planta === 'cedron' || planta === 'oregano' || planta === 'rosa del desierto' || planta === 'oreja de shrek' || planta === 'cactus piedra' || planta === 'cola de burro' || planta === 'rosario' || planta === 'kalanchoe tomentosa' || planta === 'castello' || planta === 'arbol de jade' || planta === 'perforata'){
         n = Number(prompt(`Que cantidad de plantines de ${planta} quieres?`)) 
+        while (isNaN(n)) {
+            n = Number(prompt(`DATO MAL INGRESADO!  Ingrese nuevamente la cantidad de plantines que desee?
+            Por favor ingrese un NÚMERO`)) 
+        }
+        
+
         precioI = 100    
         suma = precioI*n
         console.log(`Usted quiere ${n} plantines de ${planta}, cada uno vale $${precioI}
@@ -72,7 +114,6 @@ const compra = (planta) => {
         console.log(`Disculpe, no tenemos por el momento este tipo de producto`);
     }
 }
-
 
 // La funcion seccionTienda() es para el boton "tienda" que desplegara un submenu hacia los diferentes sectores de la tienda, recibe como parametro un numero el cual lo llevara hacia dicha zona
 const seccionTienda = b => {
@@ -93,49 +134,6 @@ const seccionTienda = b => {
     return b
 }
 
-// The function validacion() le pasa como parametro un string validado a la function inicio()
-function validacion(string) {
-    if (!string) {
-        console.log(`"${acumularInfo}" no es un carácter valido`);
-    } else if (string === 'redes' || string === 'redes sociales' || string === 'recomendacion') {
-        console.log(`Gracias! saber que usted se conecto con nosotros a través de "${acumularInfo}", nos permite seguir brindando un mejor servicio`); 
-        console.log(`Ahora..`);
-    } else if (string === 'otro'){
-        let otroLlegada = prompt(`Por favor, nos especificaria por donde conecto con nosotros`)
-        console.log(`Gracias! saber que usted se conecto con nosotros a través de "${otroLlegada}", nos permite seguir brindando un mejor servicio`); 
-        console.log(`Ahora..`);
-    }
-    return string
-}
-
-// Functon inicio() se encarga de guiar al cliente hacia su compra deseada en caso de que si conozca plantisfulsoul y en caso de que no, guarda informacion para saber como llegan los clientes al vivero y luego los invita a la "seccion nosotros" para que nos conozcan
-let acumularInfo = ''
-const inicio = () => {
-    let ask = prompt(`¿Ya nos conocias?
-    Responde por si o no`).toLowerCase()
-    if (ask === 'si') {
-        console.log(`Muy felices de que vuelvas! 
-        ahora..`);
-        return seccionTienda(numTienda())
-    } else if (ask === 'no') {
-        acumularInfo = prompt(`¿Cómo nos conoció? redes sociales, recomendacion u otro?`).toLowerCase()
-        validacion(acumularInfo)
-        nosotros()
-    } else {
-        console.log(`El caracter ingresado: "${ask}" no es valido`);
-    }
-}
-
-// function nosotros() dirige al cliente hacia la seccion "Nosotros"
-function nosotros() {
-    console.log(`Bienvenido a la seccion donde te contaremos sobre Nosotros!`);
-}
-
-// function Contacto() dirige al cliente hacia la seccion "Contacto"
-function contacto() {
-    console.log(`Bienvenido a Contacto!`);
-}
-
 // Menu = Bucle que contiene un condicional
 let salirMenu = true
 do {
@@ -143,31 +141,27 @@ do {
     1 - Inicio
     2 - Nosotros
     3 - Tienda
-    4 - Contacto
     0 - Salir del menu 
     `)
 
     switch (opcionMenu) {
         case '1':
-        console.log(`Bienvenido al Inicio!`);
-        inicio()
+            console.log(`Bienvenido al Inicio!`);
+            inicio()
         break;
         case '2':
-        nosotros()
+            nosotros()
         break;
         case '3':
-        console.log('Bienvenido a nuestra tienda!');
-        seccionTienda(numTienda())
-        break;
-        case '4':
-        contacto()
+            console.log('Bienvenido a nuestra tienda!');
+            seccionTienda(numTienda())
         break;
         case '0':
-        console.log('Salir del menu');
-        salirMenu = false
+            console.log('Salir del menu');
+            salirMenu = false
         break;
         default:
             console.log('Opcion no valida');
-            break;
+        break;
     }
 } while (salirMenu);
